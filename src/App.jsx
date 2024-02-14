e3af286b6e1f76a4e395a33d46fa216011e4a27d
Islomjon0221 <islomjon20110221@gmail.com>
2024-02-15 04:40:43 +0500

    Vazifa tayyor

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,42 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import './App.css'
 import Stack from '@mui/material/Stack';
-import man from '../public/vite.svg'
+import me from '/me.jpg'
+import { useEffect, useState } from 'react';
+import Loading from './components/loader/Loading';
+import { useTranslation } from 'react-i18next';
 function App() {
+  const {t , i18n} = useTranslation();
+  const [lang, setLang] = useState("en");
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    let lang = localStorage.getItem('lang');
+    if (lang) {
+      i18n.changeLanguage(lang);
+      setLang(lang);
+    }
+  }, [])
+
+  function handleLang(e) {
+    setLang(e.target.value);
+    i18n.changeLanguage(e.target.value)
+    localStorage.setItem('lang', e.target.value)
+  }
+    
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
   
   return (
     <>
+
    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
@@ -24,45 +55,44 @@ function App() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            MY-SITE
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>{t("web")}
           </Typography>
-          <Button color="inherit">About Me</Button>
-          <Button color="inherit">Skills</Button>
-          <Button color="inherit">Project</Button>
+          <Button color="inherit">{t('about')}</Button>
+          <Button color="inherit">{t('skills')}</Button>
+          <Button color="inherit">{t('project')}</Button>
+
+          <nav>
+            <ul>
+              <li>
+                <select value={lang} onChange={handleLang} className='typr'>
+                  <option value="en">English</option>
+                  <option value="uz">Uzbek</option>
+                  <option value="ru">Russian</option>
+                </select>
+                </li>
+            </ul>
+          </nav>
 
         </Toolbar>
       </AppBar>
     </Box>
-    <main className='container'>
+    <main className='wrapper'>
       <div>
-      <h2 className='main_divfirst_h2'>
-      Hi      ,
-I’m Islomjon,
-Front-end Developer
-      </h2>
-      <p className='main_divfirst_p'>
-      I design and develop experiences that make people’s lives simpler through Web and Mobile apps.I work with FIgma , HTML5, CSS3, JavaScript, React, ReactNative and Flutter.
-      </p>
+      <h2 className='hero'>{t('hero')}</h2>
+      <img className='img' src={me} alt="" />
+      <p className='footer'>{t('footer')}</p>
 
       
       <Stack direction="row" spacing={2}>
-      <Button variant="contained">HIRE ME</Button>
-      <Button variant="contained" href="#contained-buttons">
-      SEE MY PROJECTS
+      <Button variant="contained">{t('button1')}</Button>
+      <Button variant="contained" href="#">{t('button2')}
       </Button>
     </Stack>
 
+      <div className='butoon'>
+      </div>
+      </div>
       
-      </div>
-
-    
-
-
-
-      <div>
-      <img className='main_divsecond' src={man} alt="" />
-      </div>
     </main>
     </>
   )
---
+++ src/components/loader/Loading.jsx
@@ -0,0 +1,11 @@
+function Loading() {
+    return (
+      <div className="loader-container">
+        <div className="loader"></div>
+      </div>
+    );
+  }
+
+  
+  
+  export default Loading;
